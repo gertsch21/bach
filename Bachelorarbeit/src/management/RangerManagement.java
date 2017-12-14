8723f80d1ea46697f70873a6d3fa84946380c570
Gerhard <a01363761@unet.univie.ac.at>
2017-12-14 11:48:41 +0100

    test comPortFinder

--- Bachelorarbeit/src/management/RangerManagement.java
+++ Bachelorarbeit/src/management/RangerManagement.java
@@ -14,54 +14,55 @@ import purejavacomm.UnsupportedCommOperationException;
 import xmlmaker.RoboterWriter;
 
 /**
- * Singleton
- * Diese Klasse ist fuer die Businesslogic des Rangers vorhanden, jegliche Informationen ueber den Ranger
- * werden in dieser Klasse agglomeriert und weitergegeben, zudem koennen auch die befehle, welche der Ranger kann
+ * Singleton Diese Klasse ist fuer die Businesslogic des Rangers vorhanden,
+ * jegliche Informationen ueber den Ranger werden in dieser Klasse agglomeriert
+ * und weitergegeben, zudem koennen auch die befehle, welche der Ranger kann
  * ueber diese klasse abgegeben werden
+ * 
  * @author Gerhard
  *
  */
 public class RangerManagement {
 	MbotClient mc;
 	RoboterWriter rw;
-	private static RangerManagement instance; //singleton
-	
-	private RangerManagement() {
-	        try {
-	            // mc = new MbotClient(CommPortIdentifier.getPortIdentifier("COM6"));
-	        	this.mc = new MbotClient(CommPortIdentifier.getPortIdentifier("COM4"));
-	        } catch (PortInUseException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } catch (UnsupportedCommOperationException e) {
-	            e.printStackTrace();
-	        } catch (NoSuchPortException e) {
-	            e.printStackTrace();
-	        } 
-	        
-	        
-	        try{
-	        	this.rw = new RoboterWriter();
-	        }catch (JAXBException e) {
-	        	e.printStackTrace();
-	        }
+	private static RangerManagement instance; // singleton
 
-	        System.out.println("Ranger initialisiert");
+	private RangerManagement() {
+		for(int i=1;i<10 || this.mc!=null;i++) {
+			System.out.println("COM Port" +i);
+			try {
+				// mc = new MbotClient(CommPortIdentifier.getPortIdentifier("COM6"));
+				this.mc = new MbotClient(CommPortIdentifier.getPortIdentifier("COM"+i));
+			} catch (PortInUseException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (UnsupportedCommOperationException e) {
+				e.printStackTrace();
+			} catch (NoSuchPortException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			this.rw = new RoboterWriter();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Ranger initialisiert");
 	}
-	
+
 	public static RangerManagement getInstance() {
-		if(instance == null) 
+		if (instance == null)
 			instance = new RangerManagement();
 		return instance;
 	}
-	
-	
-	
+
 	/**
-	 * Diese Methode soll die aktuellen Daten vom Roboter in das XML File lokal zwischen speichern
-	 * NOCH NICHT FERTIG; NUR ZUM TESTEN!!!!!!!!!!!!!!!!!!!!
-	 * @throws JAXBException 
+	 * Diese Methode soll die aktuellen Daten vom Roboter in das XML File lokal
+	 * zwischen speichern NOCH NICHT FERTIG; NUR ZUM TESTEN!!!!!!!!!!!!!!!!!!!!
+	 * 
+	 * @throws JAXBException
 	 */
 	public void saveCurrentRoboterData() throws JAXBException {
 		System.out.println("Starte:RangerManagement:saveCurrentRoboterData");
@@ -74,11 +75,11 @@ public class RangerManagement {
 		myRanger.setLinefollower(hatLineFollower());
 		rw.serialize(myRanger);
 	}
-	
-	
-	
+
 	/**
-	 * soll nicht aus dem zwischengespeicherten file die Daten holen, sondern frisch vom Arduino
+	 * soll nicht aus dem zwischengespeicherten file die Daten holen, sondern frisch
+	 * vom Arduino
+	 * 
 	 * @return
 	 */
 	public Roboter getCurrentRoboterData() {
@@ -96,16 +97,15 @@ public class RangerManagement {
 		}
 		return null;
 	}
-	
-	
+
 	public String getCurrentRoboterDataAsXML() throws JAXBException {
 		System.out.println("Starte:RangerManagement:getCurrentRoboterDataAsXML");
 		return rw.objectToXMLString(getCurrentRoboterData());
 	}
-	
+
 	public Roboter getRoboterData() {
 		System.out.println("Starte:RangerManagement:getRoboterData");
-		Roboter vonXML_file=null;
+		Roboter vonXML_file = null;
 		try {
 			vonXML_file = rw.deserialize();
 		} catch (JAXBException e) {
@@ -113,33 +113,35 @@ public class RangerManagement {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
 			System.err.println("RangerManagement:getRoboterData: XML File nicht gefunden.");
-			return getCurrentRoboterData();//da noch keins vorhanden
+			return getCurrentRoboterData();// da noch keins vorhanden
 		}
-		
+
 		return vonXML_file;
 	}
-	
-	public String getRoboterDataAsXML() throws JAXBException  {
+
+	public String getRoboterDataAsXML() throws JAXBException {
 		System.out.println("Starte:RangerManagement:getRoboterDataAsXML");
 		return rw.objectToXMLString(getRoboterData());
 	}
-	
-	
+
 	private static boolean istFahrbar() {
 		return true;
 	}
-	private static  boolean hatAbstandssensor() {
+
+	private static boolean hatAbstandssensor() {
 		return false;
 	}
-	private static  boolean hatLineFollower() {
+
+	private static boolean hatLineFollower() {
 		return false;
 	}
+
 	private static String getFirma() {
 		return "Makeblock";
 	}
+
 	private static String getName() {
 		return "Makeblock Ranger";
 	}
 
-	
 }
